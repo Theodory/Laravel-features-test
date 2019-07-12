8c9814268eb3320610baec83f88d06459181743c
Theodory <theodoryf@gmail.com>
2019-07-12 16:34:09 +0300

    Add twitter bootstrap table

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -66,5 +66,41 @@
         </div>
     </div>
 </div>
+
+{{-- For Table
+   --}}
+
+   <div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('custom.welcome') }}</div>
+
+                <div class="card-body">
+                    <table class="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th scope="col">#</th>
+                          <th scope="col">Product Name</th>
+                          <th scope="col">Quantity In Stock</th>
+                          <th scope="col">Price Per Item</th>
+                          <th scope="col">Total Value Number</th>
+                      </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <th scope="row">1</th>
+                      <td>Mark</td>
+                      <td>Otto</td>
+                      <td>@mdo</td>
+                      <td>@mdo</td>
+                  </tr>
+              </tbody>
+          </table>
+      </div>
+  </div>
+</div>
+</div>
+</div>
 @endsection
 
