25ba5c6927da240173d74e5c61b02036f5c54ecd
Theodory <theodoryf@gmail.com>
2019-07-12 17:11:21 +0300

    store data in json file

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -11,6 +11,9 @@ class ProductController extends Controller
 	 * @return illuminate/Http/View
 	 */
     public function index(){
+    	$products = json_decode(file_get_contents(public_path() . '/products.json'));
+
+    	//dd($products);
     	return view('products');
     }
 
@@ -22,10 +25,19 @@ class ProductController extends Controller
      public function store(Request $request){
      	$this->validate($request,[
      		'product_name' => 'required|min:3|max:255',
-     		'quantity_in_stock' => 'required|numeric|min:3|max:255',
-     		'price_per_item' => 'required|numeric|min:3|max:255',
+     		'quantity_in_stock' => 'required|numeric',
+     		'price_per_item' => 'required|numeric',
      	]);
 
+     	//Take only form fields from the request
+     	$product = $request->only("product_name","quantity_in_stock","price_per_item");
+
+     	$today = $mytime = now()->toDateString();
+
+     	//Add today in product array
+     	 array_push($product, $today);
+     	 $product = json_encode($product);
+     	\File::put('products.json', $product);
 
 
     	return back();
