5a52cb5935379fa51a45450a3324fe693a87c4f8
Theodory <theodoryf@gmail.com>
2019-07-12 16:39:57 +0300

    validate product form fields

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,7 +6,28 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+	/**
+	 * Redirect home
+	 * @return illuminate/Http/View
+	 */
     public function index(){
     	return view('products');
+    }
+
+    /**
+     * Store Product in
+     * @param  Request $request 
+     * @return void
+     */
+     public function store(Request $request){
+     	$this->validate($request,[
+     		'product_name' => 'required|min:3|max:255',
+     		'quantity_in_stock' => 'required|digits:3|min:3|max:255',
+     		'price_per_item' => 'required|digits:3|min:3|max:255',
+     	]);
+
+
+
+    	return back();
     }
 }
